0d8e367652ad89cc1f417eef27be3b2815eec0b1
GhostSalt <ghostsalt12@gmail.com>
2026-02-17 22:04:18 +0000

    -Boykisser!

--- items/jokers/shitpost/boykisser.lua
+++ items/jokers/shitpost/boykisser.lua
@@ -1,28 +1,28 @@
-
 SMODS.Joker {
     key = "boykisser",
     name = "Boykisser",
-    pools = { shitpost = true, wip = true },
+    pools = { shitpost = true },
     atlas = "jokers",
     pos = { x = 6, y = 8 },
 
-    config = { extra = {bonus = 2} },
-    rarity = 1,
-    cost = 3,
+    config = { extra = { bonus = 1 } },
+    rarity = 2,
+    cost = 5,
     blueprint_compat = false,
-       unlocked = true,
+    unlocked = true,
     discovered = true,
     eternal_compat = true,
     perishable_compat = true,
     demicolon_compat = true,
 
     loc_vars = function(self, info_queue, card)
-        return { vars = {card.ability.extra.bonus} }
+        return { vars = { card.ability.extra.bonus } }
     end,
 
     calculate = function(self, card, context)
-
-       
-
-    end,
+        if context.individual and context.cardarea == G.play and (context.other_card:get_id() == 11 or context.other_card:get_id() == 13) then
+            context.other_card.ability.perma_mult = (context.other_card.ability.perma_mult or 0) + card.ability.extra.bonus
+            return { message = localize('k_upgrade_ex'), colour = G.C.MULT }
+        end
+    end
 }
