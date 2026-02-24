a9b8d5458f1c15735a16160120a180de369c659b
Camostar34 <jahmircallahan@cox.net>
2026-02-24 16:45:59 -0500

    chantilly cake

--- items/jokers/chantilly.lua
+++ items/jokers/chantilly.lua
@@ -4,11 +4,11 @@ SMODS.Joker {
    pronouns = "she_her",
     atlas = "jokers",
     pos = { x = 2, y = 6 },
-    pools = { wip = true},
-    config = { extra = { evil_xmult = 0, addxmult = 0.5, } },
-    rarity = 1,
-    cost = 4,
-    blueprint_compat = false,
+    pools = { },
+    config = { extra = {  } },
+    rarity = 2,
+    cost = 8,
+    blueprint_compat = true,
        unlocked = true,
     discovered = true,
     eternal_compat = true,
@@ -19,8 +19,26 @@ SMODS.Joker {
         return { vars = {} }
     end,
 
-    calculate = function(self, card, context)
-       
+   calculate = function(self, card, context)
+    
+        if context.joker_main then
+            local unique_types = {}
+            local count = 0
+            
+            -- Iterate through all held consumables
+            for _, v in ipairs(G.consumeables.cards) do
+                if v.ability and v.ability.set and not unique_types[v.ability.set] then
+                    unique_types[v.ability.set] = true
+                    count = count + 1
+                end
+            end
 
+            -- Ensure we don't multiply by 0 or 1 if unnecessary
+            if count > 1 then
+                return {
+                    xmult = count
+                }
+            end
+        end
     end,
 }
