c42565086ce5ecda87a70616b659445c5bb650eb
Camostar34 <jahmircallahan@cox.net>
2026-02-24 17:10:27 -0500

    muffin tray

--- items/jokers/muffintray.lua
+++ items/jokers/muffintray.lua
@@ -4,16 +4,16 @@ SMODS.Joker {
 
     atlas = "jokers",
     pos = { x = 5, y = 9 },
-    pools = { wip = true},
-    config = { extra = {bonus = 45} },
+    pools = {},
+    config = { extra = {} },
     rarity = 1,
-    cost = 3,
-    blueprint_compat = false,
+    cost = 5,
+    blueprint_compat = true,
        unlocked = true,
     discovered = true,
     eternal_compat = true,
     perishable_compat = true,
-    demicolon_compat = true,
+    demicolon_compat = false,
 
     loc_vars = function(self, info_queue, card)
         return { vars = {card.ability.extra.bonus} }
@@ -21,7 +21,25 @@ SMODS.Joker {
 
     calculate = function(self, card, context)
 
-       
+            if not context.end_of_round and context.individual and context.cardarea == G.hand then 
+
+            if context.other_card.config.center == G.P_CENTERS.m_bonus then
+                print(context.other_card.ability)
+            return {
+				card = other_card,
+                chips = context.other_card.ability.bonus
+            }
+            end
+
+            if context.other_card.config.center == G.P_CENTERS.m_mult then
+            return {
+				card = other_card,
+                mult = context.other_card.ability.mult
+            }
+            end
+            
+
+        end
 
     end,
 }
