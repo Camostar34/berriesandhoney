ac4d1b67fd165ec98291d052b31d6e82a35743c1
GhostSalt <ghostsalt12@gmail.com>
2026-02-26 14:03:38 +0000

    -Golden Tooth!

--- items/globals.lua
+++ items/globals.lua
@@ -1,79 +1,114 @@
 function SMODS.current_mod.calculate(self, context)
-      if context.end_of_round and context.beat_boss and not context.main_eval and not context.game_over then
+    if context.end_of_round and context.beat_boss and not context.main_eval and not context.game_over then
 
-        for _, card in ipairs(SMODS.find_card("j_smsn_bash", true)) do 
-            SMODS.debuff_card(card, false, 'smsn_bash')
+        for _, card in ipairs(SMODS.find_card("j_smsn_bash", true)) do
+            SMODS.debuff_card(card, false, "smsn_bash")
             card:juice_up(0.3, 0.4)
         end
     end
-           
+
 end
 
 local smsn_valid_glazes = {
-        "e_smsn_redglaze",
-        "e_smsn_blueglaze",
-        "e_smsn_yellowglaze",
-        "e_smsn_brownglaze",
-        "e_smsn_orangeglaze"
-    }
+    "e_smsn_redglaze", "e_smsn_blueglaze", "e_smsn_yellowglaze", "e_smsn_brownglaze", "e_smsn_orangeglaze",
+}
 
 function smsn_apply_random_glaze(card)
-    local selected_key = pseudorandom_element(smsn_valid_glazes, pseudoseed('smsn_glaze'))
+    local selected_key = pseudorandom_element(smsn_valid_glazes, pseudoseed("smsn_glaze"))
     card:set_edition(selected_key, true, true)
 end
 
 function smsn_apply_different_glaze(card)
-    if not (card.edition and G.P_CENTERS[card.edition.key] and G.P_CENTERS[card.edition.key].pools and G.P_CENTERS[card.edition.key].pools.Glaze) then return smsn_apply_random_glaze(card) end
+    if not (card.edition and G.P_CENTERS[card.edition.key] and G.P_CENTERS[card.edition.key].pools and
+        G.P_CENTERS[card.edition.key].pools.Glaze) then return smsn_apply_random_glaze(card) end
 
     local valid_glazes_temp = {}
     for _, v in ipairs(smsn_valid_glazes) do
         if v ~= card.edition.key then valid_glazes_temp[#valid_glazes_temp + 1] = v end
     end
 
-    local selected_key = pseudorandom_element(valid_glazes_temp, pseudoseed('smsn_glaze'))
+    local selected_key = pseudorandom_element(valid_glazes_temp, pseudoseed("smsn_glaze"))
     card:set_edition(selected_key, true, true)
 end
-
 
 local drpd_ref = G.FUNCS.draw_from_play_to_discard
 
 G.FUNCS.draw_from_play_to_discard = function(e)
 
-    local peanut_active = next(SMODS.find_card('j_smsn_peanutbutter'))
-    local samson_active = next(SMODS.find_card('j_smsn_samson'))
-    
-   
+    local peanut_active = next(SMODS.find_card("j_smsn_peanutbutter"))
+    local samson_active = next(SMODS.find_card("j_smsn_samson"))
+
     local play_count = #G.play.cards
     local it = 1
-    
+
     for k, v in ipairs(G.play.cards) do
         if not v.shattered and not v.destroyed then
             local return_to_hand = false
-            
-      
-            if peanut_active then 
-                return_to_hand = true 
-            end
 
-           
-            if samson_active and SMODS.has_enhancement(v, 'm_gold') then 
-                return_to_hand = true 
-            end
+            if peanut_active then return_to_hand = true end
 
-        
-            if v.edition and v.edition.key == 'e_smsn_orangeglaze' then
-                return_to_hand = true
-            end
+            if samson_active and SMODS.has_enhancement(v, "m_gold") then return_to_hand = true end
 
-           
+            if v.edition and v.edition.key == "e_smsn_orangeglaze" then return_to_hand = true end
+
             if return_to_hand then
-              
-                draw_card(G.play, G.hand, it*100/play_count, 'up', true, v)
+
+                draw_card(G.play, G.hand, it * 100 / play_count, "up", true, v)
             else
-               
-                draw_card(G.play, G.discard, it*100/play_count, 'down', false, v)
+
+                draw_card(G.play, G.discard, it * 100 / play_count, "down", false, v)
             end
             it = it + 1
         end
+    end
+end
+
+function smsn_add_gold_cards(cards, juice_card)
+    for i = 1, cards do
+        G.E_MANAGER:add_event(Event({
+            trigger = "before",
+            delay = 0.3,
+            func = function()
+                local _card = SMODS.create_card {
+                    set = "Base",
+                    area = G.discard,
+                 }
+                G.playing_card = (G.playing_card and G.playing_card + 1) or 1
+                _card.playing_card = G.playing_card
+                table.insert(G.playing_cards, _card)
+
+                G.E_MANAGER:add_event(Event({
+                    trigger = "before",
+                    delay = "0.6",
+                    func = function()
+                        _card:set_ability("m_gold")
+                        _card:start_materialize({ G.C.SECONDARY_SET.Enhanced })
+                        G.play:emplace(_card)
+                        return true
+                    end,
+                }))
+
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        juice_card:juice_up()
+                        draw_card(G.play, G.deck, 90, "up")
+                        G.GAME.blind:debuff_card(_card)
+                        G.E_MANAGER:add_event(Event({
+                            func = function()
+                                G.deck.config.card_limit = G.deck.config.card_limit + 1
+                                return true
+                            end,
+                        }))
+                        SMODS.calculate_context({
+                            playing_card_added = true,
+                            cards = { _card },
+                         })
+                        save_run()
+                        return true
+                    end,
+                }))
+                return true
+            end,
+        }))
     end
 end
--- items/jokers/goldentooth.lua
+++ items/jokers/goldentooth.lua
@@ -1,27 +1,41 @@
-SMODS.Joker { 
-
+SMODS.Joker {
     key = "goldentooth",
     name = "Golden Tooth",
     pronouns = "she_her",
     atlas = "jokers",
-    pos = { x = 0, y = 5 },
-    pools = { wip = true},
-    config = { extra = { } },
-    rarity = 2,
+    pos = {
+        x = 0,
+        y = 5,
+     },
+    config = {
+        extra = {
+            odds = 4,
+         },
+    },
+    rarity = 1,
     unlocked = true,
     discovered = true,
-    cost = 3,
-    blueprint_compat = false,
+    cost = 5,
+    blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
     demicolon_compat = true,
 
     loc_vars = function(self, info_queue, card)
-        return { vars = {} }
+        local num, denom = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, "goldentooth")
+        return {
+            vars = { num, denom },
+         }
     end,
 
     calculate = function(self, card, context)
-       
- -- Samson himself does nothing, the stupid idiot. A lovely patch is doing all the work. 
+        if context.buying_card and context.card ~= card and
+            SMODS.pseudorandom_probability(card, "goldentooth", 1, card.ability.extra.odds) then
+            smsn_add_gold_cards(1, card)
+        end
     end,
+
+    credits = {
+        code = "GhostSalt",
+     },
 }
