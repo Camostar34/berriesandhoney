eaa7bf0e1f58831e5ee0dfd3b31f65e9c8dbadc0
GhostSalt <ghostsalt12@gmail.com>
2026-03-05 17:31:36 +0000

    -Astro!

--- items/jokers/guest/astro.lua
+++ items/jokers/guest/astro.lua
@@ -5,7 +5,6 @@ SMODS.Joker {
     atlas = "jokers",
     pools = {
         guest = true,
-        wip = true
      },
     pos = {
         x = 7,
@@ -13,8 +12,10 @@ SMODS.Joker {
      },
 
     config = {
-        extra = { chips_times = 2.5 },
-     },
+        extra = {
+            chips_times = 2.5,
+         },
+    },
     rarity = 3,
     cost = 10,
     blueprint_compat = false,
@@ -29,22 +30,36 @@ SMODS.Joker {
             vars = { card.ability.extra.chips_times },
          }
     end,
-    
+
     credits = {
         character = "Astro",
         idea = "Astro",
-        code = "GhostSalt"
-    },
+        code = "GhostSalt",
+     },
 }
 
 -- Thanks, TOGA's Stuff!
 local calcindiveffectref = SMODS.calculate_individual_effect
 SMODS.calculate_individual_effect = function(effect, scored_card, key, amount, from_edition)
-    local astros = SMODS.find_card("j_smsn_astro")
-    if scored_card.config.center.set == "Joker" and next(astros) and key then
-        if string.lower(key) == "mult" or string.lower(key) == "mult_mod" then
-            key = "chips"
-            amount = amount * astros[#astros].ability.extra.chips_times
+    local me_joker = (effect.juice_card and effect.juice_card.config.center.set == "Joker" and effect.juice_card) or
+    (effect.message_card and effect.message_card.config.center.set == "Joker" and effect.message_card) or
+    (scored_card and scored_card.config.center.set == "Joker" and scored_card)
+
+    local me
+    local chosen_astro
+    if me_joker then
+        for i, v in ipairs(G.jokers.cards) do if v == me_joker then me = i end end
+        chosen_astro = (G.jokers.cards[me + 1] and G.jokers.cards[me + 1].config.center.key == "j_smsn_astro" and
+                                 G.jokers.cards[me + 1]) or
+                                 (G.jokers.cards[me - 1] and G.jokers.cards[me - 1].config.center.key == "j_smsn_astro" and
+                                     G.jokers.cards[me - 1])
+    end
+
+    if me_joker and chosen_astro and key then
+        if string.lower(key) == "mult" or string.lower(key) == "mult_mod" or string.lower(key) == "h_mult" then
+            key = string.lower(key) == "h_mult" and "h_chips" or "chips"
+            amount = amount * chosen_astro.ability.extra.chips_times
+            effect.message = nil
         end
     end
 
