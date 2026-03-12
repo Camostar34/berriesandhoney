9f2c387af18e4faeabc392186b8a9ee31efae986
Camostar34 <jahmircallahan@cox.net>
2026-03-12 01:06:31 -0400

    astro hotfix? was apparently crashing with a Blind

--- items/jokers/guest/astro.lua
+++ items/jokers/guest/astro.lua
@@ -41,25 +41,24 @@ SMODS.Joker {
 -- Thanks, TOGA's Stuff!
 local calcindiveffectref = SMODS.calculate_individual_effect
 SMODS.calculate_individual_effect = function(effect, scored_card, key, amount, from_edition)
-    local me_joker = (effect.juice_card and effect.juice_card.config.center.set == "Joker" and effect.juice_card) or
-    (effect.message_card and effect.message_card.config.center.set == "Joker" and effect.message_card) or
-    (scored_card and scored_card.config.center.set == "Joker" and scored_card)
 
-    local me
-    local chosen_astro
-    if me_joker then
-        for i, v in ipairs(G.jokers.cards) do if v == me_joker then me = i end end
-        chosen_astro = (G.jokers.cards[me + 1] and G.jokers.cards[me + 1].config.center.key == "j_smsn_astro" and
-                                 G.jokers.cards[me + 1]) or
-                                 (G.jokers.cards[me - 1] and G.jokers.cards[me - 1].config.center.key == "j_smsn_astro" and
-                                     G.jokers.cards[me - 1])
-    end
-
-    if me_joker and chosen_astro and key then
-        if string.lower(key) == "mult" or string.lower(key) == "mult_mod" or string.lower(key) == "h_mult" then
-            key = string.lower(key) == "h_mult" and "h_chips" or "chips"
-            amount = amount * chosen_astro.ability.extra.chips_times
-            effect.message = nil
+    if G.jokers and G.jokers.cards and key and (string.lower(key) == "mult" or string.lower(key) == "mult_mod" or string.lower(key) == "h_mult") then
+        
+        
+        for i = 1, #G.jokers.cards do
+            if G.jokers.cards[i] == effect.juice_card or G.jokers.cards[i] == effect.message_card or G.jokers.cards[i] == scored_card then
+                
+               
+                if (G.jokers.cards[i - 1] and G.jokers.cards[i - 1].config.center.key == "j_smsn_astro") or 
+                   (G.jokers.cards[i + 1] and G.jokers.cards[i + 1].config.center.key == "j_smsn_astro") then
+                    
+                    
+                    key = string.lower(key) == "h_mult" and "h_chips" or "chips"
+                    amount = amount * (G.jokers.cards[i - 1] and G.jokers.cards[i - 1].config.center.key == "j_smsn_astro" and G.jokers.cards[i - 1].ability.extra.chips_times or G.jokers.cards[i + 1].ability.extra.chips_times)
+                    effect.message = nil
+                end
+                break
+            end
         end
     end
 
